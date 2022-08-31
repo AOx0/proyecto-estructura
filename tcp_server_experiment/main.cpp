fc607af804b9d0e525043fbbb1d7427208bc2bb1
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-08-30 21:04:33 -0500

    Add TCP Server Experiment

---
+++ tcp_server_experiment/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+extern "C" void print(char* n);
+extern "C" void * start_server();
+extern "C" void end_server(void *);
+
+using namespace std;
+
+int main() {
+  string in;
+  //print((char *)&"Hola");
+
+  cout << "Starting server..." << endl;
+  void * server = (void *)start_server(); 
+  cout << "Started!!" << endl;
+  
+  cout << "Input anything to end server: ";
+  cin >> in;
+
+  end_server(server);
+
+  cout << "Hello World!" << endl;
+
+  std::chrono::seconds timespan(5); // or whatever
+
+  std::this_thread::sleep_for(timespan);
+  
+  return 0;
+}
