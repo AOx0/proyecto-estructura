a356e5ad79ddb4d5a43a359dd41fa1ed8342975c
Restyled.io <commits@restyled.io>
2022-09-18 22:17:39 +0000

    Restyled by clang-format

--- tcp_server_experiment/lib.h
+++ tcp_server_experiment/lib.h
@@ -4,16 +4,16 @@
 using namespace std;
 
 struct Shared {
-    uint8_t *value;
-    uint8_t typ;
-    bool null;
+  uint8_t *value;
+  uint8_t typ;
+  bool null;
 };
 
 struct Tcp {
-    uint8_t *runtime;
-    uint8_t *continue_signal;
-    uint8_t *recv_signal;
-    uint8_t *stay_alive;
+  uint8_t *runtime;
+  uint8_t *continue_signal;
+  uint8_t *recv_signal;
+  uint8_t *stay_alive;
 };
 
 extern "C" void drop_shared(Shared);
@@ -28,30 +28,28 @@ extern "C" void stop(Tcp);
 
 struct TcpServer {
 protected:
-    Tcp server;
+  Tcp server;
 
 public:
-    TcpServer() : server(start()) {}
-    ~TcpServer() {
-        stop(server);
+  TcpServer() : server(start()) {}
+  ~TcpServer() { stop(server); }
+
+  string recv() {
+    string result;
+    Shared rec = receive(server);
+
+    if (rec.null || rec.typ != 1) {
+      result = (string)NULL;
+    } else {
+      result = (char *)rec.value;
     }
 
-    string recv() {
-        string result;
-        Shared rec = receive(server);
+    drop_shared(rec);
+    return result;
+  }
 
-        if (rec.null || rec.typ != 1) {
-            result = (string)NULL;
-        } else {
-            result = (char *)rec.value;
-        }
-
-        drop_shared(rec);
-        return result;
-    }
-
-    void send(const string& msg) {
-        char * m = (char *)msg.c_str();
-        communicate(server, m);
-    }
+  void send(const string &msg) {
+    char *m = (char *)msg.c_str();
+    communicate(server, m);
+  }
 };
