69ab28a86a88ebf53b731fa03dcd6ac9b841eb32
Restyled.io <commits@restyled.io>
2022-09-18 03:55:53 +0000

    Restyled by clang-format

--- tcp_server_experiment/main.cpp
+++ tcp_server_experiment/main.cpp
@@ -1,56 +1,55 @@
+#include "server/target/cxxbridge/rust/cxx.h"
+#include "server/target/cxxbridge/server/src/lib.rs.h"
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <sstream>
 #include <thread>
 #include <vector>
-#include <cstring>
-#include "server/target/cxxbridge/server/src/lib.rs.h"
-#include "server/target/cxxbridge/rust/cxx.h"
 
 using namespace std;
 
 struct TcpServer {
 protected:
-    Tcp server;
-public:
-    TcpServer() : server(start()) {}
-    ~TcpServer() {
-        stop(server);
-    }
-    string recv() {
-        rust::String rec = receive(server);
-        string res(rec);
-        return res;
-    }
+  Tcp server;
 
-    void send(string msg) {
-        rust::String msg2(msg);
-        communicate(server, msg2);
-    }
+public:
+  TcpServer() : server(start()) {}
+  ~TcpServer() { stop(server); }
+  string recv() {
+    rust::String rec = receive(server);
+    string res(rec);
+    return res;
+  }
+
+  void send(string msg) {
+    rust::String msg2(msg);
+    communicate(server, msg2);
+  }
 };
 
-
 int main() {
-    string in;
+  string in;
 
-    cout << "Starting server..." << endl;
-    TcpServer server = TcpServer();
-    cout << "Started!!" << endl;
+  cout << "Starting server..." << endl;
+  TcpServer server = TcpServer();
+  cout << "Started!!" << endl;
 
-    string r, b;
-    stringstream a;
-    while (true) {
-        r = server.recv();
-        a.str(string());
-        a << "Hola desde C++ " << r << endl;
-        b = a.str();
-        if (r == "exit") {
-            continue;
-        }
-        server.send(b);
-        cout << "Received \"" << r << "\"" << endl;
-        if (r == "finish") break;
+  string r, b;
+  stringstream a;
+  while (true) {
+    r = server.recv();
+    a.str(string());
+    a << "Hola desde C++ " << r << endl;
+    b = a.str();
+    if (r == "exit") {
+      continue;
     }
+    server.send(b);
+    cout << "Received \"" << r << "\"" << endl;
+    if (r == "finish")
+      break;
+  }
 
-    return 0;
+  return 0;
 }
