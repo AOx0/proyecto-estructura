26120f6475fc7cb893b54a8a80eb68c6d8cd011c
Restyled.io <commits@restyled.io>
2022-09-19 08:54:44 +0000

    Restyled by astyle

--- src/main.cpp
+++ src/main.cpp
@@ -10,44 +10,44 @@ using namespace std;
 /// En caso de ingresar un numero que no está en rango, se devolverá un mensaje de error.
 /// En caso de exito, se devuelve el valor ingresado.
 Result<int, string> try_range(int from, int to) {
-  int result;
-  cin >> result;
+    int result;
+    cin >> result;
 
-  if (result >= to || result < from) {
-    return Result<int, string>::Err("Error: El valor no está en el rango especificado");
-  } else {
-    return Result<int, string>::Ok(result);
-  }
+    if (result >= to || result < from) {
+        return Result<int, string>::Err("Error: El valor no está en el rango especificado");
+    } else {
+        return Result<int, string>::Ok(result);
+    }
 }
 
 int main() {
-  DB db = DB();
+    DB db = DB();
 
-  // Option: Al igual que en rust puede ser un Some o un None.
-  // Nos sirve para respresentar la ausencia de un dato de forma explicita,
-  // así podemos hacer un mejor manejo de ausencia de datos.
-  Option<int> v = Option<int>::Some(4);
+    // Option: Al igual que en rust puede ser un Some o un None.
+    // Nos sirve para respresentar la ausencia de un dato de forma explicita,
+    // así podemos hacer un mejor manejo de ausencia de datos.
+    Option<int> v = Option<int>::Some(4);
 
-  // Lo siguiente crashearía porque no hubo un chequeo de status con v.is_some() o v.is_none()
-  // cout << "El valor es " << v.inner() << endl;
+    // Lo siguiente crashearía porque no hubo un chequeo de status con v.is_some() o v.is_none()
+    // cout << "El valor es " << v.inner() << endl;
 
-  if (v.is_some()) {
-    cout << "El valor es " << v.inner() << endl;
-  } else {
-    cout << "No hay valor" << endl;
-  }
+    if (v.is_some()) {
+        cout << "El valor es " << v.inner() << endl;
+    } else {
+        cout << "No hay valor" << endl;
+    }
 
-  // Result es una versión mejorada de Opcion que permite representar la ausencia de estado junto con
-  // un dato que indique de alguna manera qué falló.
-  // En este caso la funcion try_range puede devolver un int en caso de exito o un
-  // string con un mensaje que comunica la causa del error.
-  Result<int, string> resultado = try_range(10, 20);
+    // Result es una versión mejorada de Opcion que permite representar la ausencia de estado junto con
+    // un dato que indique de alguna manera qué falló.
+    // En este caso la funcion try_range puede devolver un int en caso de exito o un
+    // string con un mensaje que comunica la causa del error.
+    Result<int, string> resultado = try_range(10, 20);
 
-  if (resultado.is_ok()) {
-    cout << "Ingresaste el valor: " << resultado.inner() << endl;
-  } else
-    cout << resultado.inner_err() << endl;
+    if (resultado.is_ok()) {
+        cout << "Ingresaste el valor: " << resultado.inner() << endl;
+    } else
+        cout << resultado.inner_err() << endl;
 
-  return 0;
+    return 0;
 }
 
--- tcp_server_experiment/main.cpp
+++ tcp_server_experiment/main.cpp
@@ -8,25 +8,25 @@
 using namespace std;
 
 int main() {
-  string in;
+    string in;
 
-  cout << "Starting server..." << endl;
-  TcpServer server = TcpServer();
-  cout << "Started!!" << endl;
+    cout << "Starting server..." << endl;
+    TcpServer server = TcpServer();
+    cout << "Started!!" << endl;
 
-  string r;
-  stringstream a;
-  while (true) {
-    r = server.recv();
-    a.str(string());
-    a << "Hola desde C++ " << r << endl;
-    if (r == "exit") {
-      continue;
+    string r;
+    stringstream a;
+    while (true) {
+        r = server.recv();
+        a.str(string());
+        a << "Hola desde C++ " << r << endl;
+        if (r == "exit") {
+            continue;
+        }
+        server.send(a.str());
+        if (r == "finish")
+            break;
     }
-    server.send(a.str());
-    if (r == "finish")
-      break;
-  }
 
-  return 0;
+    return 0;
 }
