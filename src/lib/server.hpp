727cba7a3b009c3bd990f997962dbf774a821471
Restyled.io <commits@restyled.io>
2022-09-23 03:38:08 +0000

    Restyled by clang-format

--- src/lib/server.hpp
+++ src/lib/server.hpp
@@ -6,16 +6,16 @@
 using namespace std;
 
 struct Shared {
-    uint8_t *value;
-    uint8_t typ;
-    bool null;
-    size_t token;
+  uint8_t *value;
+  uint8_t typ;
+  bool null;
+  size_t token;
 };
 
 struct Tcp {
-    uint8_t *runtime;
-    uint8_t *recv_signal;
-    uint8_t *channels;
+  uint8_t *runtime;
+  uint8_t *recv_signal;
+  uint8_t *channels;
 };
 
 extern "C" void communicate(Tcp &, Shared &s, char *);
@@ -31,57 +31,49 @@ extern "C" void stop(Tcp);
 extern "C" void kill_sign();
 
 class Connection {
-    bool null;
-    Shared shared;
+  bool null;
+  Shared shared;
 
 public:
-    explicit Connection(Shared s) : shared(s) {
-        null = shared.null;
+  explicit Connection(Shared s) : shared(s) { null = shared.null; }
+
+  bool is_null() const { return null; }
+
+  Shared &get_shared() { return shared; }
+
+  ~Connection() {
+    if (!is_null()) {
+      drop_shared(shared);
+    }
+  }
+
+  string get_msg() const {
+    string result;
+
+    if (shared.null || shared.typ != 1) {
+      result = (string)NULL;
+    } else {
+      result = (char *)shared.value;
     }
 
-    bool is_null() const {
-        return null;
-    }
-
-    Shared &get_shared() {
-        return shared;
-    }
-
-    ~Connection() {
-        if (!is_null()) {
-            drop_shared(shared);
-        }
-    }
-
-    string get_msg() const {
-        string result;
-
-        if (shared.null || shared.typ != 1) {
-            result = (string)NULL;
-        } else {
-            result = (char *)shared.value;
-        }
-
-        return result;
-    }
+    return result;
+  }
 };
 
 class TcpServer {
 protected:
-    Tcp server;
+  Tcp server;
 
 public:
-    TcpServer() : server(start()) {}
-    ~TcpServer() {
-        stop(server);
-    }
+  TcpServer() : server(start()) {}
+  ~TcpServer() { stop(server); }
 
-    shared_ptr<Connection> recv() {
-        return make_shared<Connection>(receive(server));
-    }
+  shared_ptr<Connection> recv() {
+    return make_shared<Connection>(receive(server));
+  }
 
-    void send(Connection &s, const string &msg) {
-        char *m = (char *)msg.c_str();
-        communicate(server, s.get_shared(), m);
-    }
+  void send(Connection &s, const string &msg) {
+    char *m = (char *)msg.c_str();
+    communicate(server, s.get_shared(), m);
+  }
 };
