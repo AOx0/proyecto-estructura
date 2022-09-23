4cfed87d240d719eb9c772ff8bba60e2370f75e2
Restyled.io <commits@restyled.io>
2022-09-23 03:38:07 +0000

    Restyled by astyle

--- src/main.cpp
+++ src/main.cpp
@@ -15,49 +15,49 @@ using namespace std;
 volatile sig_atomic_t st = 0;
 
 void inthand(int signum) {
-  kill_sign();
-  st = 1;
+    kill_sign();
+    st = 1;
 }
 
 
 void resolve(shared_ptr<Connection> s, TcpServer &tcp) {
-  if (s->is_null())
-    return;
-  stringstream a((string()));
-  string r = s->get_msg();
+    if (s->is_null())
+        return;
+    stringstream a((string()));
+    string r = s->get_msg();
 
-  a << "Hola desde C++ " << r << "!" << endl;
+    a << "Hola desde C++ " << r << "!" << endl;
 
-  if (r == "slow") {
-    this_thread::sleep_for(chrono::seconds(15));
-  }
+    if (r == "slow") {
+        this_thread::sleep_for(chrono::seconds(15));
+    }
 
-  if (r == "stop")
-    inthand(0);
-  else
-    tcp.send(*s, a.str());
+    if (r == "stop")
+        inthand(0);
+    else
+        tcp.send(*s, a.str());
 }
 
 
 int main() {
-  cout << "Starting CppServer 0.1.11 ..." << endl;
-  vector<thread> threads;
+    cout << "Starting CppServer 0.1.11 ..." << endl;
+    vector<thread> threads;
 
-  signal(SIGINT, inthand);
-  signal(SIGTERM, inthand);
+    signal(SIGINT, inthand);
+    signal(SIGTERM, inthand);
 
-  {
-    TcpServer server = TcpServer();
+    {
+        TcpServer server = TcpServer();
 
-    while (!st) {
-      shared_ptr<Connection> result = server.recv();
-      threads.emplace_back(
-          thread([result, &server] { resolve(result, server); }));
+        while (!st) {
+            shared_ptr<Connection> result = server.recv();
+            threads.emplace_back(
+                thread([result, &server] { resolve(result, server); }));
+        }
     }
-  }
 
-  cout << "Finishing CPP..." << endl;
-  for (auto &t : threads) {
-    t.join();
-  }
+    cout << "Finishing CPP..." << endl;
+    for (auto &t : threads) {
+        t.join();
+    }
 }
