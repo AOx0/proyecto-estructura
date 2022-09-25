4ba6e47f5a9deec2d3a018cfc918233955b62591
Restyled.io <commits@restyled.io>
2022-09-25 22:51:56 +0000

    Restyled by clang-format

--- src/lib/table.cpp
+++ src/lib/table.cpp
@@ -1,45 +1,43 @@
 #include "table.hpp"
 
 std::vector<std::uint8_t> Table::into_vec() const {
-    std::vector<std::uint8_t> resultado {};
+  std::vector<std::uint8_t> resultado{};
 
-    for (auto & entry: rows) {
-        const char * str = entry.first.c_str();
-        size_t i = 0;
-        while (str[i] != '\0') {
-            resultado.push_back(str[i]);
-            i++;
-        }
-        resultado.push_back('\0');
-        resultado.push_back(entry.second.type);
-        resultado.push_back(entry.second.size);
-        resultado.push_back(entry.second.optional);
+  for (auto &entry : rows) {
+    const char *str = entry.first.c_str();
+    size_t i = 0;
+    while (str[i] != '\0') {
+      resultado.push_back(str[i]);
+      i++;
     }
-
     resultado.push_back('\0');
-    return resultado;
+    resultado.push_back(entry.second.type);
+    resultado.push_back(entry.second.size);
+    resultado.push_back(entry.second.optional);
+  }
+
+  resultado.push_back('\0');
+  return resultado;
 }
 
 Table Table::from_vec(const std::vector<std::uint8_t> &in) {
-    Table t { .rows = { } };
+  Table t{.rows = {}};
 
-    size_t i = 0;
+  size_t i = 0;
+  while (in[i] != '\0') {
+    std::string name;
     while (in[i] != '\0') {
-        std::string name;
-        while (in[i] != '\0') {
-            name += in[i];
-            i++;
-        }
-        Type type((Type) in[i + 1]);
-        uint8_t  size(in[i+2]);
-        bool optional(in[i+3]);
-        t.rows[name] = { .size = size, .optional = optional, .type = type  };
-        i+=4;
+      name += in[i];
+      i++;
     }
+    Type type((Type)in[i + 1]);
+    uint8_t size(in[i + 2]);
+    bool optional(in[i + 3]);
+    t.rows[name] = {.size = size, .optional = optional, .type = type};
+    i += 4;
+  }
 
-    return t;
+  return t;
 }
 
-bool Table::operator==(const Table &other) const {
-    return rows == other.rows;
-}
+bool Table::operator==(const Table &other) const { return rows == other.rows; }
--- src/lib/table.hpp
+++ src/lib/table.hpp
@@ -6,40 +6,40 @@
 #include <vector>
 
 enum Type {
-    u8 = 1,
-    u16 = 2,
-    u64 = 3,
-    i8 = 4,
-    i16 = 5,
-    i64 = 6,
-    f32 = 7,
-    f64 = 8,
-    str = 9
+  u8 = 1,
+  u16 = 2,
+  u64 = 3,
+  i8 = 4,
+  i16 = 5,
+  i64 = 6,
+  f32 = 7,
+  f64 = 8,
+  str = 9
 };
 
 struct Layout {
-    uint8_t size;
-    bool optional;
-    Type type;
+  uint8_t size;
+  bool optional;
+  Type type;
 
-    bool operator==(Layout const & other) const {
-        int result = 0;
-        result += size == other.size;
-        result += optional == other.optional;
-        result += type == other.type;
+  bool operator==(Layout const &other) const {
+    int result = 0;
+    result += size == other.size;
+    result += optional == other.optional;
+    result += type == other.type;
 
-        return result;
-    }
+    return result;
+  }
 };
 
 struct Table {
-    std::map<std::string, Layout> rows;
+  std::map<std::string, Layout> rows;
 
-    std::vector<std::uint8_t> into_vec() const;
+  std::vector<std::uint8_t> into_vec() const;
 
-    static Table from_vec(std::vector<std::uint8_t> const & in);
+  static Table from_vec(std::vector<std::uint8_t> const &in);
 
-    bool operator==(Table const & other) const;
+  bool operator==(Table const &other) const;
 };
 
-#endif //TABLE_HPP
+#endif // TABLE_HPP
