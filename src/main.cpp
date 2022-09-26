9230e22cad13532742851899b0b2842fa1c602b4
Restyled.io <commits@restyled.io>
2022-09-26 20:04:39 +0000

    Restyled by clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <csignal>
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <sstream>
 #include <thread>
 #include <vector>
-#include <optional>
 
 #include "lib/server.hpp"
 
@@ -14,50 +14,50 @@ using namespace std;
 volatile sig_atomic_t st = 0;
 
 void inthand(int signum) {
-    kill_sign();
-    st = 1;
+  kill_sign();
+  st = 1;
 }
 
 void resolve(shared_ptr<optional<Connection>> s, TcpServer &tcp) {
-    stringstream a((string()));
-    std::optional<string> r = s->value().get_msg();
+  stringstream a((string()));
+  std::optional<string> r = s->value().get_msg();
 
-    if (r.has_value()) {
-        string & query = r.value();
+  if (r.has_value()) {
+    string &query = r.value();
 
-        // Here goes the query handling
+    // Here goes the query handling
 
-        // For example, if it is equal to "stop", we turn down the server
-        if (r == "stop")
-            inthand(0);
-        else
-            tcp.send(s->value(), a.str());
-    } else {
-        tcp.send(s->value(), "Error getting message");
-    }
+    // For example, if it is equal to "stop", we turn down the server
+    if (r == "stop")
+      inthand(0);
+    else
+      tcp.send(s->value(), a.str());
+  } else {
+    tcp.send(s->value(), "Error getting message");
+  }
 }
 
 int main() {
-    cout << "Starting CppServer 0.1.11 ..." << endl;
-    vector<thread> threads;
+  cout << "Starting CppServer 0.1.11 ..." << endl;
+  vector<thread> threads;
 
-    signal(SIGINT, inthand);
-    signal(SIGTERM, inthand);
+  signal(SIGINT, inthand);
+  signal(SIGTERM, inthand);
 
-    {
-        TcpServer server = TcpServer();
+  {
+    TcpServer server = TcpServer();
 
-        while (!st) {
-            shared_ptr<optional<Connection>> event = server.recv();
-            if (event->has_value()) {
-                threads.emplace_back(
-                    thread([event, &server] { resolve(event, server); }));
-            }
-        }
+    while (!st) {
+      shared_ptr<optional<Connection>> event = server.recv();
+      if (event->has_value()) {
+        threads.emplace_back(
+            thread([event, &server] { resolve(event, server); }));
+      }
     }
+  }
 
-    cout << "Finishing CPP..." << endl;
-    for (auto &t : threads) {
-        t.join();
-    }
+  cout << "Finishing CPP..." << endl;
+  for (auto &t : threads) {
+    t.join();
+  }
 }
