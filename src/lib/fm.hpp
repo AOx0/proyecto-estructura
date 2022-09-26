b71ee3bf7ec1b2ed818f2a05bbd6feb10f3e9eb6
Restyled.io <commits@restyled.io>
2022-09-26 01:32:33 +0000

    Restyled by astyle

--- src/lib/fm.hpp
+++ src/lib/fm.hpp
@@ -6,8 +6,8 @@
 #include <vector>
 
 namespace FileManager {
-  std::vector<uint8_t> read_to_vec(const std::string & path);
-  void write_to_file(const std::string & path, const std::vector<uint8_t> & contents);
+std::vector<uint8_t> read_to_vec(const std::string & path);
+void write_to_file(const std::string & path, const std::vector<uint8_t> & contents);
 }
 
 #endif //FM_HPP
