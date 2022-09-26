14777ede5e58c335a4a21dadf1b56985e063f3bf
Restyled.io <commits@restyled.io>
2022-09-26 20:04:37 +0000

    Restyled by astyle

--- src/lib/fm.cpp
+++ src/lib/fm.cpp
@@ -1,32 +1,32 @@
 #include "fm.hpp"
 
 std::vector<uint8_t> FileManager::read_to_vec(const std::string &path) {
-  std::fstream file;
-  file.open(path, std::ios::in | std::ios::binary);
-  file.unsetf(std::ios::skipws);
+    std::fstream file;
+    file.open(path, std::ios::in | std::ios::binary);
+    file.unsetf(std::ios::skipws);
 
-  std::streampos fileSize;
+    std::streampos fileSize;
 
-  file.seekg(0, std::ios::end);
-  fileSize = file.tellg();
-  file.seekg(0, std::ios::beg);
+    file.seekg(0, std::ios::end);
+    fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
 
-  std::vector<uint8_t> vec;
-  vec.reserve(fileSize);
+    std::vector<uint8_t> vec;
+    vec.reserve(fileSize);
 
-  vec.insert(vec.begin(), std::istream_iterator<uint8_t>(file),
-             std::istream_iterator<uint8_t>());
+    vec.insert(vec.begin(), std::istream_iterator<uint8_t>(file),
+               std::istream_iterator<uint8_t>());
 
-  file.close();
-  return vec;
+    file.close();
+    return vec;
 }
 
 void FileManager::write_to_file(const std::string &path,
                                 const std::vector<uint8_t> &contents) {
-  std::ofstream file;
-  file.open(path, std::ios::out | std::ios::binary);
+    std::ofstream file;
+    file.open(path, std::ios::out | std::ios::binary);
 
-  file.write(reinterpret_cast<const char *>(contents.data()), contents.size());
+    file.write(reinterpret_cast<const char *>(contents.data()), contents.size());
 
-  file.close();
+    file.close();
 }
