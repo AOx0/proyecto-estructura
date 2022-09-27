e3e78e8d52a72ba446672fedeaf8e50cb321802b
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-09-26 20:10:54 -0500

    Impl db tables SerDe

--- src/lib/database.hpp
+++ src/lib/database.hpp
@@ -1,6 +1,18 @@
 #ifndef DATABASE_HPP
 #define DATABASE_HPP
 
-struct DataBase {};
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+#include "table.hpp"
+
+struct DataBase {
+  std::vector<std::string> tables;
+
+  std::vector<std::uint8_t> into_vec() const;
+};
+
+
 
 #endif // DATABASE_HPP
---
+++ tests/database.cc
@@ -0,0 +1,14 @@
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <vector>
+
+#include "../src/lib/database.hpp"
+
+TEST(DB_SerDe, StructToVec) {
+  std::vector<std::uint8_t> expected { 't', 'a', 'b', 'l', 'a', '_', '1', '\0', 't', 'a', 'b', 'l', 'a', '_', '2', '\0', '\0' }; 
+  
+  DataBase input { .tables = { "tabla_1", "tabla_2" } };
+  std::vector<std::uint8_t> result(input.into_vec());
+  
+  EXPECT_EQ(expected, result);
+}
