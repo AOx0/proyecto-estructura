ecfd2db3855d76abdcb8b890519c2e219a8a53cd
Restyled.io <commits@restyled.io>
2022-09-28 23:58:42 +0000

    Restyled by clang-format

--- src/lib/database.hpp
+++ src/lib/database.hpp
@@ -8,17 +8,17 @@
 #include "table.hpp"
 
 struct DataBase {
-    std::vector<std::string> tables;
+  std::vector<std::string> tables;
 
-    std::vector<std::uint8_t> into_vec() const;
+  std::vector<std::uint8_t> into_vec() const;
 
-    static DataBase from_vec(const std::vector<std::uint8_t> &in);
+  static DataBase from_vec(const std::vector<std::uint8_t> &in);
 
-    static DataBase from_file(const std::string & path);
+  static DataBase from_file(const std::string &path);
 
-    void to_file(const std::string &path) const;
+  void to_file(const std::string &path) const;
 
-    bool operator==(const DataBase &other) const;
+  bool operator==(const DataBase &other) const;
 };
 
 #endif // DATABASE_HPP
