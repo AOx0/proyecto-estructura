4e9c04b14bbce11c0552f511e99cf037ade44c73
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-10-06 20:53:41 -0500

    Improve db client code

--- toi/src/main.rs
+++ toi/src/main.rs
@@ -1,69 +1,58 @@
-use std::{io::{Write, Read}, process::exit};
-use text_io::*;
 use clap::Parser;
-use std::net::{TcpStream, SocketAddr};
+use std::net::TcpStream;
+use std::{
+    io::{Read, Write},
+    process::exit,
+};
+use text_io::*;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about = "DB Client for Toi", long_about = None)]
 struct Args {
     /// The ip to connect to
-    #[arg(short, long, default_value = "127.0.0.1")]
+    #[arg(required = true)]
     ip: String,
     /// The port to connect to
-    #[arg(short, long, default_value = "9999")]
-    port: String
+    #[arg(required = true)]
+    port: String,
 }
 
-fn main()  {
+fn run() -> Result<(), Box<dyn std::error::Error>> {
     let Args { ip, port } = Args::parse();
 
-    let address = format!("{}:{}", ip, port);
-    let address: SocketAddr = address.parse().unwrap();
-   
-        let stream = TcpStream::connect(address);
+    let mut stream = TcpStream::connect(format!("{ip}:{port}"))?;
 
-        if stream.is_err() {
-            println!("Error while connecting to tcp: {}", stream.unwrap_err().to_string());
-            exit(1);
+    let mut msg: String;
+    let mut buf;
+    loop {
+        buf = vec![0; 512 * 4];
+        msg = try_read!("{}\n")?;
+
+        if !msg.is_ascii() {
+            println!("Message must be ascii");
+            continue;
         }
 
-        let mut stream = stream.unwrap();
- 
-        loop {
-            let mut buf = [0; 512*4];
+        let _ = stream.write(msg.as_bytes())?;
 
-            let message: Result<String, _> = try_read!("{}\n");
-
-            if let Ok(msg) = message {
-                if !msg.is_ascii() {
-                    continue;
-                }
-
-                let bytes = msg.as_bytes();
-
-                // Send message
-
-                if let Ok(_) = stream.write(bytes) {
-
-                    if msg == "exit" || msg == "stop" {
-                        break;
-                    }
-                    // Receive response
-                    if let Ok(_) = stream.read(&mut buf) {
-
-                        let response = String::from_utf8(buf.to_vec());
-
-                        if response.is_ok() {
-                            print!("{}", response.unwrap());
-                        }
-                    } else {
-                        println!("Error while reading from TcpStream. Is the server on?");
-                    }
-                } else {
-                    println!("Error while writing to TcpStream. Is the server on?");
-                    continue;
-                }
-            }
+        if msg == "exit" || msg == "stop" {
+            break;
         }
+
+        let _ = stream.read(&mut buf)?;
+
+        let response = String::from_utf8(buf.to_vec())?;
+
+        print!("{response}");
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(error) = run() {
+        println!("Error: {error}");
+        exit(1);
+    }
 }
